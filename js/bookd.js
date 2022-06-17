c194dd4a05ecbeceda4fd3c39be93f05d5d045c6
MeGaXV <abdosasuke@gmail.com>
2022-06-18 00:20:35 +0200

    Book details working very good

--- js/bookd.js
+++ js/bookd.js
@@ -7,3 +7,20 @@ $.ajax({
         console.log(res)
     }
 })
+var params = location.search.substring(1).split("&")
+var param={"id":""}
+param.id=parseInt(params[0].split("=")[1])
+console.log(param)
+var list=document.querySelectorAll("li[id]")
+var author = document.getElementById('authpub')
+var auth = list[param.id-1].nextElementSibling.nextElementSibling.innerText
+var img = document.getElementById("cover")
+img.setAttribute("src", list[parseInt(param.id)-1].nextElementSibling.innerText)
+var pub = list[param.id-1].nextElementSibling.nextElementSibling.nextElementSibling.innerText
+author.innerHTML= "Author: "+ auth +"<br/> Publisher: " + pub
+var abs = document.getElementById("abstract")
+abs.innerText = list[param.id-1].nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.innerText
+var title = document.getElementById("bookTitle")
+title.innerText=list[param.id-1].innerText
+var stock= document.getElementById("availability")
+stock.innerText="In Stock: "+list[param.id-1].nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.innerText
