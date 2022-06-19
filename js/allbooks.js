38581a9602d50a8aae48b24fda88b8f432a11b14
Saif Abuelhasan <saif@DESKTOP-BD63ERL.localdomain>
2022-06-19 17:33:52 +0300

    Finished search

--- js/allbooks.js
+++ js/allbooks.js
@@ -3,26 +3,29 @@ var seeDetails= function(id){
     location.href="book-details.html?id="+ id
 }
 var dlist= document.getElementById("list")
+var searchParam = window.location.search.replace("?q=", "").replaceAll("%20", " ").toLowerCase()
 for(i=0; i<list.length; i++){
     var book = list[i]
-    var div = document.createElement('div')
-    div.className="details"
-    var img= document.createElement('div')
-    img.className="image"
-    img.innerHTML="<img src="+book.nextElementSibling.innerText+" class='cover'/>"
-    var about = document.createElement("div")
-    about.className="authpub"
-    about.innerHTML = "Title: "+ book.innerText+ "<br/> Author: "+book.nextElementSibling.nextElementSibling.innerText+"<br/> Publisher: "+ book.nextElementSibling.nextElementSibling.nextElementSibling.innerText
-    
-    var button = document.createElement("input")
-    button.setAttribute("type","button")
-    button.setAttribute("value","Read More")
-    console.log(book.id)
-    button.setAttribute("onclick","seeDetails("+book.id+")")
-    div.appendChild(img)
-    div.appendChild(about)
-    div.appendChild(button)
-    dlist.appendChild(div)
+    if (book.innerText.toLowerCase().startsWith(searchParam)) {
+        var div = document.createElement('div')
+        div.className="details"
+        var img= document.createElement('div')
+        img.className="image"
+        img.innerHTML="<img src="+book.nextElementSibling.innerText+" class='cover'/>"
+        var about = document.createElement("div")
+        about.className="authpub"
+        about.innerHTML = "Title: "+ book.innerText+ "<br/> Author: "+book.nextElementSibling.nextElementSibling.innerText+"<br/> Publisher: "+ book.nextElementSibling.nextElementSibling.nextElementSibling.innerText
+        
+        var button = document.createElement("input")
+        button.setAttribute("type","button")
+        button.setAttribute("value","Read More")
+        console.log(book.id)
+        button.setAttribute("onclick","seeDetails("+book.id+")")
+        div.appendChild(img)
+        div.appendChild(about)
+        div.appendChild(button)
+        dlist.appendChild(div)
+    }
 }
 
 
--- search.js
+++ search.js
@@ -38,15 +38,13 @@ inputBox.onkeyup = (e)=>{
 function select(element){
     let selectData = element.textContent;
     inputBox.value = selectData;
-    icon.onclick = ()=>{
-        //webLink = `https://www.google.com/search?q=${selectData}`;
-        //linkTag.setAttribute("href", webLink);
-        //linkTag.click();
-        
-
-    }
     searchWrapper.classList.remove("active");
 }
+
+icon.addEventListener("click", () => {
+    console.log(inputBox.value)
+    window.location.href = `./all-books.html?q=${inputBox.value}`
+})
 
 function showSuggestions(list){
     let listData;
